ee1a1128afb1ec307a4a7de2bad0537972b38e4d
WebGeek <3159382965@qq.com>
2022-09-03 15:13:19 +0800

    完成“登录授权”

--- pages/login/index.js
+++ pages/login/index.js
@@ -7,7 +7,41 @@ Page({
   data: {
 
   },
-
+  getLogin(){
+    wx.login({
+      success(res){
+        if(res.code){
+          console.log("返回code===》",res.code);
+        }else{        
+          console.log("登录失败",res.errMsg);
+        }
+      }
+    })
+  },
+  // onLoad: function () {
+  //   // 获取用户openid
+  //   let that = this
+  //   wx.login({
+  //       success: function (res) { //请求自己后台获取用户openid
+  //           console.log(res.code)
+  //           wx.request({
+  //               url: '开发者后台接口',
+  //               data: {
+  //                   appid: '自己的微信公众号获取',
+  //                   secret: '自己的微信公众号获取',
+  //                   code: res.code
+  //               },
+  //               success: function (response) {
+  //                   var openid = response.data.openid;
+  //                   console.log('请求获取openid:' + openid); //可以把openid存到本地，方便以后调用
+  //                   wx.setStorageSync('openid', openid);
+  //                   that.setData({
+  //                       openid: "获取到的openid：" + openid
+  //                   })
+  //               }
+  //           })
+  //       }
+  //   })
   /**
    * 生命周期函数--监听页面加载
    */
