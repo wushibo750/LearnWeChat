2c3a78095ad8125781fbba0417de344fab719ad2
WebGeek <3159382965@qq.com>
2022-09-11 20:48:07 +0800

    完成"TDesign UI布局2"

--- pages/layout/tdesigner/tdesigner.js
+++ pages/layout/tdesigner/tdesigner.js
@@ -9,7 +9,36 @@ Page({
     border: {
       color: '#f6f6f6',
     },
-    value:3
+    value:3,
+    originFiles: [
+      {
+        url: 'https://tdesign.gtimg.com/site/upload1.png',
+        name: 'uploaded1.png',
+        type: 'image',
+      },
+    ],
+    gridConfig: {
+      column: 4,
+      width: 160,
+      height: 160,
+    },
+    config: {
+      count: 1,
+    },
+  },
+  handleSuccess(e) {
+    const { files } = e.detail;
+    this.setData({
+      originFiles: files,
+    });
+  },
+  handleRemove(e) {
+    const { index } = e.detail;
+    const { originFiles } = this.data;
+    originFiles.splice(index, 1);
+    this.setData({
+      originFiles,
+    });
   },
   onChange(e){
     console.log(e)
