fc37f189b7458c968c1e94d83061880c5bfdf5f5
WebGeek <3159382965@qq.com>
2022-09-12 15:03:11 +0800

    完成"TDesign组件xu"

---
+++ components/radio/radio.js
@@ -0,0 +1,26 @@
+// components/radio/radio.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onChange(event) {
+      console.log("获取数据===》",event)
+      console.log('radio', event.detail);
+    },
+  }
+})
